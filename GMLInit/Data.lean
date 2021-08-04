3ffbcaac4a873902c66dd782f92ac5ced6a6a4d4
François G. Dorais <fgdorais@gmail.com>
2021-08-04 07:59:43 -0400

    add Data/HVal

--- GMLInit/Data.lean
+++ GMLInit/Data.lean
@@ -1,3 +1,4 @@
 import GMLInit.Data.Basic
 import GMLInit.Data.Equiv
+import GMLInit.Data.HVal
 import GMLInit.Data.Index
---
+++ GMLInit/Data/HVal.lean
@@ -0,0 +1,25 @@
+import GMLInit.Data.Basic
+
+inductive HVal.{u} : Type u
+| mk {α : Sort u} : α → HVal
+
+protected def HVal.Sort : HVal → Type _
+| @mk α _ => α
+
+protected def HVal.val : (a : HVal) → a.Sort
+| mk a => a
+
+theorem HVal.eq_of_val_heq_val : {a b : HVal} → a.val ≅ b.val → a = b
+| mk _, mk _, HEq.refl _ => Eq.refl _
+
+theorem HVal.val_heq_val_of_eq : {a b : HVal} → a = b → a.val ≅ b.val
+| mk _, mk _, Eq.refl _ => HEq.refl _
+
+theorem HVal.eq_iff_val_heq_val (a b : HVal) : a = b ↔ a.val ≅ b.val :=
+  ⟨HVal.val_heq_val_of_eq, HVal.eq_of_val_heq_val⟩
+
+theorem HVal.proof_irrel : (a b : HVal.{0}) → a = b
+| @mk a ha, @mk b hb =>
+  have h : a = b := propext ⟨λ _ => hb, λ _ => ha⟩
+  match a, b, h with
+  | a, .(a), rfl => proofIrrel ha hb ▸ rfl
