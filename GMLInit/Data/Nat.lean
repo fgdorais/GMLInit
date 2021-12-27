974a477295a02fb67e50305d2db2bc4684c7898b
François G. Dorais <fgdorais@gmail.com>
2021-12-27 17:25:11 -0500

    add Data/Nat/Pos

--- GMLInit/Data/Nat.lean
+++ GMLInit/Data/Nat.lean
@@ -2,6 +2,7 @@ import GMLInit.Data.Nat.Add
 import GMLInit.Data.Nat.Basic
 import GMLInit.Data.Nat.Mul
 import GMLInit.Data.Nat.Order
+import GMLInit.Data.Nat.Pos
 import GMLInit.Data.Nat.Pow
 import GMLInit.Data.Nat.Sub
 import GMLInit.Data.Nat.Succ
