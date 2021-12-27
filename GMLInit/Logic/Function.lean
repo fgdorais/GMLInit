4d23fd96ffcda5aa72e5eaa43ef4a90f255b1b57
François G. Dorais <fgdorais@gmail.com>
2021-12-27 09:50:21 -0500

    feat Logic/Function
    update `Injective` classes and instances

--- GMLInit/Logic/Function.lean
+++ GMLInit/Logic/Function.lean
@@ -11,32 +11,31 @@ abbrev dcurry {α} {β : α → Sort _} {γ : (a : α) × β a → Sort _} (f : 
 abbrev duncurry {α} {β : α → Sort _} {γ : {a : α} → β a → Sort _} (f : (a : α) → (b : β a) → γ b) (ab : (a : α) × β a) : γ ab.snd := f ab.fst ab.snd
 
 class InjectiveT {α β} (f : α → β) : Prop where
-  protected elim {a₁ a₂} : f a₁ = f a₂ → a₁ = a₂
+  protected inj {a₁ a₂} : f a₁ = f a₂ → a₁ = a₂
 
-class Injective {α β} (f : α → β) extends InjectiveT f : Prop
+export InjectiveT (inj)
 
-protected instance Injective.id {α} : Injective (@id α) where
-  elim := id
+abbrev injection {α β} (f : α → β) [InjectiveT f] {lhs rhs : α} : f lhs = f rhs → lhs = rhs := inj
 
-protected instance InjectiveT.comp {α β γ} (f : α → β) (g : β → γ) [InjectiveT f] [Injective g] : InjectiveT (g ∘ f) where
-  elim {x₁ x₂} h := InjectiveT.elim (InjectiveT.elim h)
-
-protected def Injective.comp {α β γ} (f : α → β) (g : β → γ) [Injective f] [Injective g] : Injective (g ∘ f) where
-  toInjectiveT := inferInstance
-
-theorem injection {α β} (f : α → β) [InjectiveT f] {a₁ a₂ : α} : f a₁ = f a₂ → a₁ = a₂ := InjectiveT.elim
-
-theorem injectionIff {α β} (f : α → β) [InjectiveT f] {a₁ a₂ : α} : f a₁ = f a₂ ↔ a₁ = a₂ := Iff.intro InjectiveT.elim (congrArg f)
+theorem injectionIff {α β} (f : α → β) [InjectiveT f] {a₁ a₂ : α} : f a₁ = f a₂ ↔ a₁ = a₂ := Iff.intro inj (congrArg f)
 
 theorem injectionEq {α β} (f : α → β) [InjectiveT f] {a₁ a₂ : α} : (f a₁ = f a₂) = (a₁ = a₂) := propext (injectionIff f)
 
-instance : Injective Nat.succ where elim := Nat.succ.inj
-instance (α) : Injective (@some α) where elim := Option.some.inj
-instance (α β) : Injective (@Sum.inl α β) where elim := Sum.inl.inj
-instance (α β) : Injective (@Sum.inr α β) where elim := Sum.inr.inj
-instance (a : α) : Injective (a::.) where elim := λ h => (List.cons.inj h).2
-instance (as : List α) : Injective (.::as) where elim := λ h => (List.cons.inj h).1
-instance (α β) (a : α) : Injective (Prod.mk a : β → α × β) where elim := λ h => (Prod.mk.inj h).2
-instance (α β) (b : β) : Injective (Prod.mk . b : α → α × β) where elim := λ h => (Prod.mk.inj h).1
+class Injective {α β} (f : α → β) extends InjectiveT f : Prop
+
+protected instance InjectiveT.comp {α β γ} (g : α → β) (f : β → γ) [Injective f] [InjectiveT g] : InjectiveT (f $ g .) where
+  inj {x₁ x₂} h := inj (inj h)
+
+instance (α) : Injective (id : α → α) where inj := id
+instance (α β) : Injective (Sum.inl : α → Sum α β) where inj := Sum.inl.inj
+instance (α β) : Injective (Sum.inr : β → Sum α β) where inj := Sum.inr.inj
+instance (α β) (a : α) : Injective ((a,.) : β → α × β) where inj := λ h => (Prod.mk.inj h).right
+instance (α β) (b : β) : Injective ((.,b) : α → α × β) where inj := λ h => (Prod.mk.inj h).left
+instance (a : α) : Injective (a::.) where inj := λ h => (List.cons.inj h).2
+instance (as : List α) : Injective (.::as) where inj := λ h => (List.cons.inj h).left
+instance (α) : Injective (some : α → Option α) where inj := Option.some.inj
+instance : Injective Nat.succ where inj := Nat.succ.inj
+instance (n : Nat) : Injective (n+.: Nat → Nat) where inj := Nat.add_left_cancel
+instance (n : Nat) : Injective (.+n: Nat → Nat) where inj := Nat.add_right_cancel
 
 end Function
