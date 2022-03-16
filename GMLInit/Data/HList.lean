d91b2b3db52be93d024078650367d6b312368f12
François G. Dorais <fgdorais@gmail.com>
2022-03-16 00:46:58 -0400

    feat Data/HList
    add overloaded list notations

--- GMLInit/Data/HList.lean
+++ GMLInit/Data/HList.lean
@@ -9,13 +9,20 @@ inductive HList.{u} : List (Sort u) → Type u
 | nil : HList []
 | cons {α αs} : α → HList αs → HList (α :: αs)
 
+infixr:67 " :: " => HList.cons
+syntax (name := hlist) "[" term,* "]"  : term
+macro_rules (kind := hlist)
+  | `([ ])           => `(HList.nil)
+  | `([ $a ])        => `(HList.cons $a HList.nil)
+  | `([ $a, $as,* ]) => `(HList.cons $a [$as,*])
+
 namespace HList
 
 protected def extAux : {αs βs : List (Sort _)} → (as : HList αs) → (bs : HList βs) → List Prop
-| [], [], nil, nil => []
-| [], _::_, nil, cons _ _ => [False]
-| _::_, [], cons _ _ , nil => [False]
-| _::_, _::_, cons a as, cons b bs => (a ≅ b) :: HList.extAux as bs
+| [], [], [], [] => []
+| [], _::_, [], _::_ => [False]
+| _::_, [],  _::_, [] => [False]
+| _::_, _::_, a::as, b::bs => (a ≅ b) :: HList.extAux as bs
 
 protected theorem ext {αs : List (Sort _)} (as₁ as₂ : HList αs) : All (HList.extAux as₁ as₂) → as₁ = as₂ := by
   intro h
@@ -38,24 +45,24 @@ protected theorem dext {αs βs : List (Sort _)} (as : HList αs) (bs : HList β
   exact hs
 
 protected def mk : {αs : List (Sort _)} → ((i : Index αs) → i.val) → HList αs
-| [], _ => nil
-| α::αs, v => cons (v Index.head) (HList.mk λ i => v (Index.tail i))
+| [], _ => []
+| α::αs, v => v .head :: HList.mk λ i => v (.tail i)
 
 protected def ofList {α} : (as : List α) → HList (as.map λ _ => α)
-| [] => nil
-| a::as => cons a (HList.ofList as)
+| [] => []
+| a::as => a :: HList.ofList as
 
 protected def ofListHVal : (vs : List HVal) → HList (vs.map HVal.Sort)
-| [] => nil
-| v::vs => cons v.val (HList.ofListHVal vs)
+| [] => []
+| v::vs => v.val :: HList.ofListHVal vs
 
 def append : {αs βs : List (Sort _)} → HList αs → HList βs → HList (αs ++ βs)
-| [], βs, nil, bs => bs
-| α :: αs, βs, cons a as, bs => List.cons_append α αs βs ▸ cons a (append as bs)
+| [], βs, [], bs => bs
+| α :: αs, βs, a::as, bs => List.cons_append α αs βs ▸ cons a (append as bs)
 
 def eval : {αs : List (Sort _)} → HList αs → (i : Index αs) → i.val
-| _::_, cons a _, Index.head => a
-| _::_, cons _ as, Index.tail i => eval as i
+| _::_, a::_, .head => a
+| _::_, _::as, .tail i => eval as i
 
 def equiv (αs : List (Sort _)) : Equiv (HList αs) ((i : Index αs) → i.val) where
   fwd := HList.eval
