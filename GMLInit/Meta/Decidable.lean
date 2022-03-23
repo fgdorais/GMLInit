fbc390921a4dfa874b91593cd85356b1a731d9ef
François G. Dorais <fgdorais@gmail.com>
2022-03-23 17:27:54 -0400

    fix Meta/Decidable
    improve `by_cases` tactic

--- GMLInit/Meta/Decidable.lean
+++ GMLInit/Meta/Decidable.lean
@@ -9,11 +9,8 @@ macro_rules
 | `(tactic|truth_table $p:term $ps:term*) =>
   `(tactic|induction ($p : Prop) using Decidable.casesTFOn <;> truth_table $ps*)
 
-syntax by_cases_true := "|" &"isTrue" (ident <|> "_") "=>" (hole <|> syntheticHole <|> tacticSeq)
-syntax by_cases_false := "|" &"isFalse" (ident <|> "_") "=>" (hole <|> syntheticHole <|> tacticSeq)
-syntax "by_cases" term "with" withPosition((colGe by_cases_true) (colGe by_cases_false)) : tactic
-macro_rules
-| `(tactic|by_cases $p with | isTrue $ht => $tt | isFalse $hf => $tf) =>
-  `(tactic|match inferInstanceAs (Decidable $p) with | Decidable.isTrue $ht => $tt | Decidable.isFalse $hf => $tf)
+-- From Mario Carneiro <https://leanprover.zulipchat.com/#narrow/stream/270676-lean4/topic/Improving.20this.20syntax/near/276313034>
+macro "by_cases" ps:term,* "with" alts:Lean.Parser.Tactic.matchAlts : tactic =>
+  `(tactic| match $[inferInstanceAs (Decidable $ps)],* with $alts:matchAlts)
 
 end Meta
