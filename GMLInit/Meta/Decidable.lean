65666e3f41055dee3136eafa2044f8ac387430c0
François G. Dorais <fgdorais@gmail.com>
2022-04-09 03:32:54 -0400

    fix Meta/Decidable
    improve `truth_table` tactic

--- GMLInit/Meta/Decidable.lean
+++ GMLInit/Meta/Decidable.lean
@@ -3,11 +3,10 @@ import GMLInit.Meta.Basic
 
 namespace Meta
 
-syntax "truth_table" term:max* : tactic
+syntax (name := truth_table) "truth_table" (colGt term),+ : tactic
 macro_rules
-| `(tactic|truth_table) => `(tactic|simp)
-| `(tactic|truth_table $p:term $ps:term*) =>
-  `(tactic|induction ($p : Prop) using Decidable.casesTFOn <;> truth_table $ps*)
+| `(tactic| truth_table $p:term) => `(tactic| induction ($p:term : Prop) using Decidable.casesTFOn <;> skip)
+| `(tactic| truth_table $p:term,$ps:term,*) => `(tactic| induction ($p:term : Prop) using Decidable.casesTFOn <;> truth_table $ps,*)
 
 -- From Mario Carneiro <https://leanprover.zulipchat.com/#narrow/stream/270676-lean4/topic/Improving.20this.20syntax/near/276313034>
 macro "by_cases" ps:term,* "with" alts:Lean.Parser.Tactic.matchAlts : tactic =>
