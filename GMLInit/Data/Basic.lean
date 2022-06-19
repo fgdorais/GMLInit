514c5784a7f9c1278e377b1181b413c72e6bcae4
François G Dorais <fgdorais@gmail.com>
2022-06-18 20:52:38 -0400

    update Data/Basic
    add `Ordering.opp` and lemmas

--- GMLInit/Data/Basic.lean
+++ GMLInit/Data/Basic.lean
@@ -1,6 +1,26 @@
 import GMLInit.Prelude
 
+namespace Ordering
+
 deriving instance DecidableEq for Ordering
+
+def opp : Ordering → Ordering
+| lt => gt
+| eq => eq
+| gt => lt
+
+theorem opp_opp (x) : opp (opp x) = x := by cases x <;> rfl
+
+theorem opp_inj {x y} : opp x = opp y → x = y := by
+  intro h
+  rw [←opp_opp x, ←opp_opp y, h]
+
+theorem eq_opp_iff_opp_eq (x y) : opp x = y ↔ x = opp y := by
+  constructor
+  · intro h; rw [←h, opp_opp]
+  · intro h; rw [h, opp_opp]
+
+end Ordering
 
 structure Equiv.{u,v} (α : Sort u) (β : Sort v) : Sort (max 1 (max u v)) where
   fwd : α → β
