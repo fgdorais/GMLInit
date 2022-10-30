849fee98b1300df3e4e3b0c95b10bb1e9ba9adf7
François G. Dorais <fgdorais@gmail.com>
2022-10-29 21:25:18 -0400

    update Lean

--- lakefile.lean
+++ lakefile.lean
@@ -8,4 +8,4 @@ package GMLInit {
 @[default_target]
 lean_lib GMLInit {}
 
-require std from git "https://github.com/leanprover/std4.git"@"68c404a3bc72a44559fb58ae9ab74196abf9853d"
+require std from git "https://github.com/leanprover/std4.git"@"439263f585961c94df05c181616b1561063d8f3f"
