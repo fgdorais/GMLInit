b336a4cd609ec519411176bf9ce311014c0f5eea
F. G. Dorais <fgdorais@gmail.com>
2023-03-26 05:04:41 -0400

    add Data/Equiv/Quotient

--- GMLInit/Data/Equiv.lean
+++ GMLInit/Data/Equiv.lean
@@ -3,6 +3,7 @@ import GMLInit.Data.Equiv.Fun
 import GMLInit.Data.Equiv.List
 import GMLInit.Data.Equiv.Option
 import GMLInit.Data.Equiv.Prod
+import GMLInit.Data.Equiv.Quotient
 import GMLInit.Data.Equiv.Sigma
 import GMLInit.Data.Equiv.Subtype
 import GMLInit.Data.Equiv.Sum
