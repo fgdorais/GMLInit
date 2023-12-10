50072da30cef794463bc05f103b1f6cf18e03022
F. G. Dorais <fgdorais@gmail.com>
2023-12-10 02:52:25 -0500

    cleanup prelude

--- GMLInit/Prelude.lean
+++ GMLInit/Prelude.lean
@@ -2,13 +2,6 @@ import Std
 import Extra
 import Logic
 
-instance (α) [Subsingleton α] : DecidableEq α
-| _, _ => isTrue (Subsingleton.allEq _ _)
-
-class Inv (α : Type _) where
-  inv : α → α
-postfix:max "⁻¹" => Inv.inv
-
 class Apart (α : Type _) where
   apart : α → α → Prop
 infix:50 " ≶ " => Apart.apart
